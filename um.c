027d358c6308b331156aaf9c2abfb2a8209f6c86
SamBroner727 <sbro727@gmail.com>
2014-11-19 17:21:39 -0500

    put alu functions in if else

--- um.c
+++ um.c
@@ -5,135 +5,128 @@
 
 
 
-void initialize(FILE *fp, int* programCounter, uint32_t* registers);
-int execute(int* programCounter, uint32_t* registers);
-
-typedef struct three_regs {
-        uint32_t a;
-        uint32_t b;
-        uint32_t c;
-} three_regs;
-
+void initialize(FILE *fp, int* programCounter);
+int execute(int* programCounter);
 
 /* Conditional Move */
-static inline int cmov(three_regs regs)
-{
+// static inline int cmov(three_regs regs)
+// {
 
-        if (*(regs.c) != 0) {
-                *(regs.a) = *(regs.b);
-        }
+//         if (*(regs.c) != 0) {
+//                 *(regs.a) = *(regs.b);
+//         }
 
-        return 0;
-}
+//         return 0;
+// }
 
-/* Segmented Load */
-static inline int sload(three_regs regs)
-{
-        *(regs.a) = getWord(*(regs.b), *(regs.c));
-        return 0;
-}
+// /* Segmented Load */
+// static inline int sload(three_regs regs)
+// {
+//         *(regs.a) = getWord(*(regs.b), *(regs.c));
+//         return 0;
+// }
 
-/* Segmented Store */
-static inline int sstore(three_regs regs)
-{
-        return putWord(*(regs.c), *(regs.a), *(regs.b));
-}
+// /* Segmented Store */
+// static inline int sstore(three_regs regs)
+// {
+//         return putWord(*(regs.c), *(regs.a), *(regs.b));
+// }
 
-/* Addition */
-static inline int add(three_regs regs)
-{
-        *(regs.a) = (*(regs.b) + *(regs.c));
-        return 0;
-}
+// /* Addition */
+// static inline int add(three_regs regs)
+// {
+//         *(regs.a) = (*(regs.b) + *(regs.c));
+//         return 0;
+// }
 
-/* Multiplication */
-static inline int mult(three_regs regs)
-{
-        *(regs.a) = (*(regs.b) * *(regs.c));
-        return 0;
-}
+// /* Multiplication */
+// static inline int mult(three_regs regs)
+// {
+//         *(regs.a) = (*(regs.b) * *(regs.c));
+//         return 0;
+// }
 
-/* divsion */
-static inline int divide(three_regs regs)
-{
-        *(regs.a) = *(regs.b) / *(regs.c);
-        return 0;
-}
+// /* divsion */
+// static inline int divide(three_regs regs)
+// {
+//         *(regs.a) = *(regs.b) / *(regs.c);
+//         return 0;
+// }
 
-/* Bitwise NAND */
-static inline int nand(three_regs regs)
-{
-        *(regs.a) = ~(*(regs.b) & *(regs.c));
-        return 0;
-}
+// /* Bitwise NAND */
+// static inline int nand(three_regs regs)
+// {
+//         *(regs.a) = ~(*(regs.b) & *(regs.c));
+//         return 0;
+// }
 
-/* Halt */
-static inline int halt(three_regs regs)
-{
-        (void) regs;
-        return 1;
-}
+// /* Halt */
+// static inline int halt(three_regs regs)
+// {
+//         (void) regs;
+//         return 1;
+// }
 
-/* Map Segment */
-static inline int map(three_regs regs)
-{
-        uint32_t tempid = newSegment(*(regs.c));
+// /* Map Segment */
+// static inline int map(three_regs regs)
+// {
+//         uint32_t tempid = newSegment(*(regs.c));
 
-        if (tempid != 0) {
-                *(regs.b) = tempid;
-                return 0;
-        }
-        return 1;
-}
+//         if (tempid != 0) {
+//                 *(regs.b) = tempid;
+//                 return 0;
+//         }
+//         return 1;
+// }
 
-/* Unmap Segment */
-static inline int unmap(three_regs regs)
-{
-        return(removeSegment(*(regs.c)));
-}
+// /* Unmap Segment */
+// static inline int unmap(three_regs regs)
+// {
+//         return(removeSegment(*(regs.c)));
+// }
 
-/* UMOutput */
-static inline int ALUoutput(three_regs regs)
-{
-        IOoutput(*(regs.c));
-        return 0;
+// /* UMOutput */
+// static inline int ALUoutput(three_regs regs)
+// {
+//         IOoutput(*(regs.c));
+//         return 0;
 
-}
+// }
 
-/* Input */
-static inline int ALUinput(three_regs regs)
-{
-        uint32_t input = IOinput();
-        if (input == ~(uint32_t)0) {
-                *(regs.c) = ~0;
-                return 0;
-        } else if (input <= 255) {
-                *(regs.c) = input;
-                return 0;
-        }
-        return 1;
-}
+// /* Input */
+// static inline int ALUinput(three_regs regs)
+// {
+//         uint32_t input = IOinput();
+//         if (input == ~(uint32_t)0) {
+//                 *(regs.c) = ~0;
+//                 return 0;
+//         } else if (input <= 255) {
+//                 *(regs.c) = input;
+//                 return 0;
+//         }
+//         return 1;
+// }
 
-/* Load Program */
-static inline int loadp(three_regs regs, int *pc)
-{
-        if (*(regs.b) == 0) {
-                *pc = *(regs.c);
-                return 0;
-        } else{
-                replaceSegment(*(regs.b), 0);
-                *pc = *(regs.c);
-                return 0;
-        }
-        return 1;
-}
+// /* Load Program */
+// static inline int loadp(three_regs regs, int *pc)
+// {
+//         if (*(regs.b) == 0) {
+//                 *pc = *(regs.c);
+//                 return 0;
+//         } else{
+//                 replaceSegment(*(regs.b), 0);
+//                 *pc = *(regs.c);
+//                 return 0;
+//         }
+//         return 1;
+// }
 
 /* Load Value */
-static inline int loadv(uint32_t* regA, uint32_t value)
-{
-        *regA = value;
-        return 0;
-}
+// static inline int loadv(uint32_t* regA, uint32_t value)
+// {
+//         *regA = value;
+//         return 0;
+// }
 
 
 int main(int argc, char* argv[]) {
@@ -218,48 +211,64 @@ int execute(int* pc)
                 if(opcode == 13) {
                         uint32_t lreg = instruction;
                         uint32_t lval = instruction;
-                        program_status = loadv(&(registers[((lreg << 4) >> 29)]),  (lval << 7) >> 7); 
+                        registers[((lreg << 4) >> 29)] =  ((lval << 7) >> 7); 
                 } else {
                         uint32_t a = instruction;
                         uint32_t b = instruction;
                         uint32_t c = instruction;
-                        
-                        three_regs registersUsed;
 
-                        registersUsed.a = &(registers[(a << 23) >> 29]);
-                        registersUsed.b = &(registers[(b << 26) >> 29]);
-                        registersUsed.c = &(registers[(c << 29) >> 29]);
-
-                        registersUsed.a = (a << 23) >> 29;
-                        registersUsed.b = (b << 26) >> 29;
-                        registersUsed.c = (c << 29) >> 29;
+                        a = (a << 23) >> 29;
+                        b = (b << 26) >> 29;
+                        c = (c << 29) >> 29;
                         
                         if(opcode == 1) {
-                                program_status = (sload(registersUsed));
+                                registers[a] = getWord(registers[b], registers[c]);
                         } else if (opcode == 2){
-                                program_status = (sstore(registersUsed));
+                                program_status = putWord(registers[c], registers[a], registers[b]);
                         } else if (opcode == 6) {
-                                program_status = (nand(registersUsed));
+                                registers[a] = ~(registers[b] & registers[c]);
                         } else if (opcode == 12) {
-                                program_status = (loadp(registersUsed, pc));
+                                if (registers[b] == 0) {
+                                        *pc = registers[c];
+                                } else{
+                                        replaceSegment(registers[b], 0);
+                                        *pc = registers[c];
+                                }
                         } else if(opcode == 3) {
-                                program_status = (add(registersUsed));
+                                registers[a] = registers[b] + registers[c];
                         } else if(opcode == 0) {
-                                program_status = (cmov(registersUsed));
+                                if (registers[c] != 0) {
+                                        registers[a] = registers[b];
+                                }
                         } else if (opcode == 8) {
-                                program_status = (map(registersUsed));
+                                uint32_t tempid = newSegment(registers[c]);
+
+                                if (tempid != 0) {
+                                        registers[b] = tempid;
+                                        program_status = 0;
+                                }
+                                program_status = 1;
                         } else if (opcode == 9) {
-                                program_status = (unmap(registersUsed));
+                                program_status = removeSegment(registers[c]);
                         } else if (opcode == 5) {
-                                program_status = (divide(registersUsed));
+                                registers[a] = registers[b]/registers[c];
                         } else if (opcode == 4) {
-                                program_status = (mult(registersUsed));
+                                registers[a] = registers[b] * registers[c];
                         } else if (opcode == 10) {
-                                program_status = (ALUoutput(registersUsed));
+                                IOoutput(registers[c]);
                         } else if (opcode == 7) {
-                                program_status = (halt(registersUsed));
+                                program_status = 1;
                         } else if (opcode == 11) {
-                                program_status = (ALUinput(registersUsed));
+                                uint32_t input = IOinput();
+                                if (input == ~(uint32_t)0) {
+                                        registers[c] = ~0;
+                                        program_status = 0;
+                                } else if (input <= 255) {
+                                        registers[c] = input;
+                                        program_status = 0;
+                                } else {
+                                        program_status = 1;
+                                }
                         } else {
                                 program_status =  1;
                         }
